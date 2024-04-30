7da408d1fb4a93947cfae9367ac995d656103447
Alexander <159568561+Alsam91@users.noreply.github.com>
2024-04-30 23:23:47 +0300

    Update Additional_task.sql

--- Additional_task.sql
+++ Additional_task.sql
@@ -1,7 +1,12 @@
+CREATE TABLE IF NOT EXISTS departments (
+	id serial PRIMARY KEY, 
+	name varchar NOT NULL,
+);
+
 CREATE TABLE IF NOT EXISTS employees (
 	id serial PRIMARY KEY, 
 	name varchar NOT NULL,
-	department varchar,
+	department_id integer REFERENCES departments(id),
 	chief_id integer NULL,
 	FOREIGN KEY (chief_id) REFERENCES employees(id)
 );
